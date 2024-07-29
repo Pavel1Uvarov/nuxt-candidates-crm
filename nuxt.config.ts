ebb4eb5bf25c342f043ae63eb04608fc8ce9ceb2
Pavel1Uvarov <pavel2uvaorv@gmail.com>
2024-07-29 04:52:56 +0200

    feat: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,17 +25,6 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    storage: {
-      redis: {
-        driver: 'redis',
-        port: 6379,
-        host: "127.0.0.1",
-        username: "",
-        password: "",
-        db: 0,
-        tls: {}
-      }
-    },
     devStorage: {
       db: {
         driver: 'fs',
